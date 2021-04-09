6d7b9f6a957b4da3d4287024e438a7f4d045fb59
barbisliboni <niinaliboni@gmail.com>
2021-04-08 21:47:50 -0300

    Testing some commands on JS

---
+++ frontEnd/js/for.js
@@ -0,0 +1,8 @@
+function main() {
+    var x;
+    var t = 5;
+    for (x = 1; x <= 10; x++) {
+        document.write("<br>", t, " * ",  x, " = ", x*t);
+    }
+    document.write("Valor final de x: " + x)
+}
