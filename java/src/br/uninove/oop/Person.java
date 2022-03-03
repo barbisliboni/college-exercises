bcdfc2d2bf7224f662ec41efcec66990ccdc5637
barbisliboni <niinaliboni@gmail.com>
2022-03-03 20:05:21 -0300

    creating java project and writing the first line

---
+++ java/src/br/uninove/oop/Person.java
@@ -0,0 +1,15 @@
+package br.uninove.oop;
+
+public class Person {
+    public static void main(String args[]){
+
+        String name = "Bárbara";
+        String cpf = "111.111.111.11";
+        int age = 19;
+        char gender = 'F';
+        float weight = 69;
+        boolean smoker = false;
+
+        System.out.println(name);
+    }
+}
